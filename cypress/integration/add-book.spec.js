e61a737a81556b69a8bbd1b9a205dbb1bc5aca8b
Felipe Castillo <jfcastillo1204@gmail.com>
2021-03-15 01:20:52 -0500

    test for add book ui

---
+++ cypress/integration/add-book.spec.js
@@ -0,0 +1,15 @@
+const faker = require('faker').random;
+const bookName = faker.words(1)
+const bookAuthor = "FelipeCastillo"
+describe("When the user want to register a new book", () => {
+    before(() => {
+        cy.visit("https://librarycicd-ui.herokuapp.com/dashboard");
+        cy.get('.table-button.ant-btn.ant-btn-primary').click()
+        cy.get('#name').type("a", { waitForAnimations: true })
+        cy.get('#author').type(bookAuthor)
+        cy.get('.ant-btn.ant-btn-primary.ng-star-inserted').click()
+    });
+    it("The the book should be listed with the right name and author", () => {
+        cy.get('table').contains('td', 'a').should('be.visible')
+    });
+});
